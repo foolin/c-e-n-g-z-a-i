9bb4462fc7a8c146bb730625982931355918b39a
lingliufu@gmail.com <lingliufu@gmail.com@24a72b4c-9f40-9b9b-9d45-d5d727bfa653>
2012-01-17 09:11:40 +0000

    完善页面.

--- CengZai.Web/Controllers/ArticleController.cs
+++ CengZai.Web/Controllers/ArticleController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CengZai.Web.Code;
+using CengZai;
 
 namespace CengZai.Web.Controllers
 {
@@ -28,13 +29,52 @@ namespace CengZai.Web.Controllers
         [AuthorizedFilter]
         public ActionResult Post()
         {
+            string strType = (Request["type"] + "").Trim().ToLower();
+
+            if (strType == Model.ArtType.Audio.ToString().ToLower())
+            {
+                return View("PostAudio");
+            }
+            else if (strType == Model.ArtType.Image.ToString().ToLower())
+            {
+                return View("PostImage");
+            }
+            else if (strType == Model.ArtType.Link.ToString().ToLower())
+            {
+                return View("PostLink");
+            }
+            else if (strType == Model.ArtType.Video.ToString().ToLower())
+            {
+                return View("PostVideo");
+            }
+            //else if (strType == Model.ArtType.Weibo.ToString().ToLower())
+            //{
+            //    return View("PostText");
+            //}
+            else if (strType == Model.ArtType.Text.ToString().ToLower())
+            {
+                //文章
+                return View();
+            }
             return View();
         }
 
         [HttpPost]
         [AuthorizedFilter]
-        public ActionResult Post()
+        public ActionResult Post(string title, string content, string type)
         {
+            if (Request.IsAjaxRequest())
+            {
+                return AlertAndBack("Ajax提交！");
+            }
+            else
+            {
+                return AlertAndBack("非Ajax提交！");
+            }
+            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(content))
+            {
+                return AlertAndBack("标题或者内容至少1一项不能为空！");
+            }
             return View();
         }
 
