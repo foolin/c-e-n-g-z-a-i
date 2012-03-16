2872bd589e439d525eab84eef0117fd6a638af95
lingliufu@gmail.com <lingliufu@gmail.com@24a72b4c-9f40-9b9b-9d45-d5d727bfa653>
2012-03-16 08:59:29 +0000

    新增完善个人档案UI。

---
+++ branches/site/CengZai.Web/Controllers/SettingsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CengZai.Web.Common;
+
+namespace CengZai.Web.Controllers
+{
+    public class SettingsController : BaseController
+    {
+        //
+        // GET: /Settings/
+
+        public ActionResult Index()
+        {
+            Model.User user = GetLoginUser();
+            ViewBag.User = user;
+
+            return View();
+        }
+
+        //个人资料
+        [CheckAuthFilter]
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+
+        // 修改密码
+        [CheckAuthFilter]
+        public ActionResult Password()
+        {
+            return View();
+        }
+
+
+        //上传头像
+        [CheckAuthFilter]
+        public ActionResult Avatar()
+        {
+            return View();
+        }
+
+    }
+}
