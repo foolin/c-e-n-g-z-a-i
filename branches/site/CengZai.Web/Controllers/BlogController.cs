1ed2a3c06934141f90f5bc25d90ce83fdf452605
lingliufu@gmail.com <lingliufu@gmail.com@24a72b4c-9f40-9b9b-9d45-d5d727bfa653>
2012-03-16 07:58:05 +0000

    新增私信发送页面。

---
+++ branches/site/CengZai.Web/Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CengZai.Web.Controllers
+{
+    public class BlogController : Controller
+    {
+        //
+        // GET: /Blog/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //单博客
+        public ActionResult Lover(int? loverid)
+        {
+            return View();
+        }
+
+        //单身博客
+        public ActionResult User(int userid)
+        {
+            return View();
+        }
+
+    }
+}
