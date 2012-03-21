a1b02e893f5d632a2965e0a038b33642ecb4a8e5
lingliufu@gmail.com <lingliufu@gmail.com@24a72b4c-9f40-9b9b-9d45-d5d727bfa653>
2012-03-21 12:26:39 +0000

    完善相关功能。

--- branches/site/CengZai.Core/MvcExtend/HtmlExtend.cs
+++ branches/site/CengZai.Core/MvcExtend/HtmlExtend.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Text.RegularExpressions;
+using System.Collections;
 
 namespace System.Web.Mvc
 {
@@ -171,6 +173,113 @@ namespace System.Web.Mvc
             }
             return content;
         }
+
+
+        /// <summary>
+        /// 获取移除Html内容
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static MvcHtmlString GetAutoHtmlString(this HtmlHelper helper, string content, int length)
+        {
+            if (content == null || content.Length < 0)
+            {
+                return MvcHtmlString.Create(content);
+            }
+            content = CengZai.Helper.Util.RemoveHtml(content) + "";
+            if (content.Length > length)
+            {
+                content = content.Substring(0, length);
+            }
+            return MvcHtmlString.Create(content);
+        }
+
+
+        /// <summary>
+        /// 按字节长度截取字符串(支持截取带HTML代码样式的字符串)
+        /// </summary>
+        /// <param name="param">将要截取的字符串参数</param>
+        /// <param name="length">截取的字节长度</param>
+        /// <param name="endAppend">字符串末尾补上的字符串</param>
+        /// <returns>返回截取后的字符串</returns>
+        public static MvcHtmlString GetHtmlSubstring(this HtmlHelper helper, string param, int length)
+        {
+            string Pattern = null;
+            MatchCollection m = null;
+            StringBuilder result = new StringBuilder();
+            int n = 0;
+            char temp;
+            bool isCode = false; //是不是HTML代码
+            bool isHTML = false; //是不是HTML特殊字符,如&nbsp;
+            char[] pchar = param.ToCharArray();
+            for (int i = 0; i < pchar.Length; i++)
+            {
+                temp = pchar[i];
+                if (temp == '<')
+                {
+                    isCode = true;
+                }
+                else if (temp == '&')
+                {
+                    isHTML = true;
+                }
+                else if (temp == '>' && isCode)
+                {
+                    n = n - 1;
+                    isCode = false;
+                }
+                else if (temp == ';' && isHTML)
+                {
+                    isHTML = false;
+                }
+
+                if (!isCode && !isHTML)
+                {
+                    n = n + 1;
+                    //UNICODE码字符占两个字节
+                    if (System.Text.Encoding.Default.GetBytes(temp + "").Length > 1)
+                    {
+                        n = n + 1;
+                    }
+                }
+
+                result.Append(temp);
+                if (n >= length)
+                {
+                    break;
+                }
+            }
+            //取出截取字符串中的HTML标记
+            string temp_result = result.ToString().Replace("\n", "").Replace("\r", "");
+            temp_result = Regex.Replace(temp_result, @"(>)[^<>]*(<?)", @"$1$2", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            //去掉不需要结素标记的HTML标记
+            //temp_result = temp_result.Replace(@"</?(AREA|BASE|BASEFONT|BODY|BR|COL|COLGROUP|DD|DT|FRAME|HEAD|HR|HTML|IMG|INPUT|ISINDEX|LI|LINK|META|OPTION|P|PARAM|TBODY|TD|TFOOT|TH|THEAD|TR|area|base|basefont|body|br|col|colgroup|dd|dt|frame|head|hr|html|img|input|isindex|li|link|meta|option|p|param|tbody|td|tfoot|th|thead|tr)[^<>]*/?>","");
+            temp_result = Regex.Replace(temp_result
+                , @"</?(AREA|BASE|BASEFONT|BODY|BR|COL|COLGROUP|DD|DT|FRAME|HEAD|HR|HTML|IMG|INPUT|ISINDEX|LI|LINK|META|OPTION|P|PARAM|TBODY|TD|TFOOT|TH|THEAD|TR|area|base|basefont|body|br|col|colgroup|dd|dt|frame|head|hr|html|img|input|isindex|li|link|meta|option|p|param|tbody|td|tfoot|th|thead|tr)[^<>]*/?>"
+                , ""
+                , RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            //去掉成对的HTML标记
+            //temp_result = temp_result.Replace(@"<([a-zA-Z]+)[^<>]*>(.*?)</\1>", "$2");
+            temp_result = Regex.Replace(temp_result, @"<([a-zA-Z]+)[^<>]*>(.*?)</\1>", "$2", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            //用正则表达式取出标记
+            Pattern = ("<([a-zA-Z]+)[^<>]*>");
+            m = Regex.Matches(temp_result, Pattern);
+            ArrayList endHTML = new ArrayList();
+            foreach (Match mt in m)
+            {
+                endHTML.Add(mt.Result("$1"));
+            }
+            //补全不成对的HTML标记
+            for (int i = endHTML.Count - 1; i >= 0; i--)
+            {
+                result.Append("</");
+                result.Append(endHTML[i]);
+                result.Append(">");
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+
 
 
         /// <summary>
