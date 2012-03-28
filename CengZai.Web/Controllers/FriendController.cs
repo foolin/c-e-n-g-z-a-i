098992c1712f36955d8da8dce076d8ea4357c14e
lingliufu@gmail.com <lingliufu@gmail.com@24a72b4c-9f40-9b9b-9d45-d5d727bfa653>
2012-03-28 07:50:37 +0000

    添加第一次关注用户。

--- CengZai.Web/Controllers/FriendController.cs
+++ CengZai.Web/Controllers/FriendController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using CengZai.Web.Common;
 using CengZai.Helper;
+using System.Data;
 
 namespace CengZai.Web.Controllers
 {
@@ -57,6 +58,82 @@ namespace CengZai.Web.Controllers
             }
         }
 
+        /// <summary>
+        /// 关注朋友
+        /// </summary>
+        /// <returns></returns>
+        [CheckAuthFilter]
+        public ActionResult FeedFriendsForFirstLogin()
+        {
+            try
+            {
+                Model.User loginUser = GetLoginUser();
+                if (loginUser == null)
+                {
+                    return RedirectToAction("Login", "User");
+                }
+                BLL.User bllUser = new BLL.User();
+                DataSet dsList = bllUser.GetList(100, "State=1", "Vip DESC, RegTime DESC");
+                if (dsList != null && dsList.Tables.Count > 0)
+                {
+                    ViewBag.UserList = bllUser.DataTableToList(dsList.Tables[0]);
+                }
+                else
+                {
+                    return JumpToAction("暂无用户", "即将跳转到完善资料页面...", "Avatar", "Settings");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.AddErrorInfo("批量关注朋友页面出现异常！", ex);
+                return JumpToAction("页面出现异常", "即将跳转到完善资料页面...", "Avatar", "Settings");
+            }
+            return View();
+        }
+
+
+        /// <summary>
+        /// 关注朋友处理
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [CheckAuthFilter]
+        public ActionResult FeedFriendsForFirstLogin(int[] userid)
+        {
+            try
+            {
+                int feedCount = 0;
+                Model.User loginUser = GetLoginUser();
+                if (loginUser == null)
+                {
+                    return RedirectToAction("Login", "User");
+                }
+                if (userid != null && userid.Length > 0)
+                {
+                    BLL.Friend bllFriend = new BLL.Friend();
+                    foreach (int friendUserID in userid)
+                    {
+                        if (bllFriend.Add(loginUser.UserID, friendUserID))
+                        {
+                            feedCount++;
+                        }
+                    }
+                }
+                if (feedCount > 0)
+                {
+                    return JumpToAction("关注成功", "恭喜您，关注成功，一共关注" + feedCount + "个好友，您以后可以随时关注更多或者取消！", "Avatar", "Settings", null, 5);
+                }
+                else
+                {
+                    return JumpToAction("关注失败", "对不起，关注" + feedCount + "个用户！", "Avatar", "Settings", null, 5);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.AddErrorInfo("FeedFriendsForFirstLogin(int[] userid)关注用户提交失败！", ex);
+                return JumpToAction("关注失败", "对不起，关注出现异常", "FeedFriendsForFirstLogin", null, 5);
+            }
+        }
 
 
     }
