f258acd7262ee4be11af0e5080430652189b554f
lingliufu@gmail.com <lingliufu@gmail.com@24a72b4c-9f40-9b9b-9d45-d5d727bfa653>
2012-04-06 14:50:04 +0000

    新增资源管理

---
+++ CengZai.Web/resource/js/core.js
@@ -0,0 +1,46 @@
+﻿var $FR = {
+    addSuccess: function succ(json) {
+        json = eval(json);
+        if (!json) {
+            alert("操作异常！");
+            return;
+        }
+        if (json.id > 0) {
+            $("a[frienduserid='" + json.id + "']").replaceWith('<a class=" muted" data-ajax="true" data-ajax-failure="$FR.delFail" data-ajax-success="$FR.delSuccess" frienduserid="' + json.id + '" href="/friend/frienddel?frienduserid=' + json.id + '&amp;time=' + new Date().getMilliseconds() + '" title="点击取消关注">已关注</a>');
+            return;
+        }
+        else {
+            alert(json.msg);
+            return;
+        }
+    },
+    addFail: function (res) {
+        alert("操作失败！出现异常：" + res.responseText);
+    },
+    delSuccess: function succ(json) {
+        json = eval(json);
+        if (!json) {
+            alert("操作异常！");
+            return;
+        }
+        if (json.id > 0) {
+            $("a[frienduserid='" + json.id + "']").replaceWith('<a class="" data-ajax="true" data-ajax-failure="$FR.addFail" data-ajax-success="$FR.addSuccess" frienduserid="' + json.id + '" href="/friend/friendadd?frienduserid=' + json.id + '&amp;time=' + new Date().getMilliseconds() + '" title="点击关注Ta">关注Ta</a>');
+            return;
+        }
+        else {
+            alert(json.msg);
+            return;
+        }
+    },
+    delFail: function (res) {
+        alert("操作失败！出现异常：" + res.responseText);
+    }
+};
+
+
+(function (d) {
+    d['okValue'] = '确定';
+    d['cancelValue'] = '取消';
+    d['title'] = '消息';
+    // [more..]
+})($.dialog.defaults);
