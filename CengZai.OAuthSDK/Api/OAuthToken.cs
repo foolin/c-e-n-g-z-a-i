6b416887ae0054e4fb8cec26cbfa1b0904da3b2d
lingliufu@gmail.com <lingliufu@gmail.com@24a72b4c-9f40-9b9b-9d45-d5d727bfa653>
2012-04-26 10:07:25 +0000

    新增qq空间接口。

---
+++ CengZai.OAuthSDK/Api/OAuthToken.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CengZai.OAuthSDK.Api
+{
+    /// <summary>
+    /// OAuth2.0
+    /// </summary>
+    [Serializable]
+    public class OAuthToken
+    {
+        /// <summary>
+        /// access token
+        /// </summary>
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// 用户唯一标识
+        /// </summary>
+        public string Uid { get; set; }
+
+        /// <summary>
+        /// 过期时间
+        /// </summary>
+        public int ExpiresAt { get; set; }
+
+    }
+}
---
+++ CengZai.OAuthSDK/Sina/SinaConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CengZai.OAuthSDK.Api;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace CengZai.OAuthSDK.Sina
+{
+    class SinaConfig : IConfig
+    {
+        private NameValueCollection SinaSection = (NameValueCollection)ConfigurationManager.GetSection("OAuthGroup/SinaSection");
+
+        public bool IsOpen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetBaseUrl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAppKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAppSecret()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Uri GetCallbackUrl()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
